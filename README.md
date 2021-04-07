4d0a1b7856de2d82fd0b00bcfad16d07d02372e8
William King <williamking9772@gmail.com>
2021-04-07 11:17:29 +0100

    used inheritance with base.html

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # MVC with Flask
 Flask is a microframework. LinkedIn uses Flask.
+
+## Installing Flask
+ * `pip install flask`
+
---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template
+
+# Creating an app instance
+app = Flask(__name__)
+
+# Use a decorator @ to define the route for our webpage
+@app.route("/")
+
+# Setting up a default page
+def index():
+    return render_template("index.html")
+
+@app.route("/welcome/")
+def welcome():
+    return render_template("welcome.html")
+
+# Create 2 more pages/routes and add the functionality for email and password
+# Implement OOP and inheritance
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+
+
+
+
+
+
+
+
+
